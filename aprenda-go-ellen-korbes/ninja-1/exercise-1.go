4376c916aab352d0a21cd53672c192a6a1e1ba10
revoltaxz <guilhermerevolta@gmail.com>
2024-04-26 08:51:53 -0300

    First exercise - level ninja 1

---
+++ aprenda-go-ellen-korbes/ninja-1/exercise-1.go
@@ -0,0 +1,22 @@
+
+// Utilizando o operador curto de declaração, atribua estes valores às variáveis com os identifcadores "x", "y", e "z".
+// 1 - 42
+// 2 - "James Bond"
+// 3 - true
+// Desafio - Mostre o valor de cada identificador em uma linha separada.
+// Desafio - Mostre os valores em uma unica linha.
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
